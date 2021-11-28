c1c9685ce0a0ab940c3b15166601f70daf572596
Roman Avdeev <reedemer@yandex.ru>
2021-11-28 21:56:25 +0300

    borrowing

---
+++ Assets/_Ai/Scripts/Enemy.cs
@@ -0,0 +1,54 @@
+using UnityEngine;
+
+namespace BattleScripts
+{
+    internal class Enemy : IEnemy
+    {
+        private const float _kMoney = 5f;
+        private const float _kPower = 1.5f;
+        private const float _maxHealthPlayer = 20;
+
+        private readonly string _name;
+
+        private int _moneyPlayer;
+        private int _healthPlayer;
+        private int _powerPlayer;
+
+
+        public Enemy(string name) =>
+            _name = name;
+
+
+        public void Update(DataPlayer dataPlayer)
+        {
+            switch (dataPlayer.DataType)
+            {
+                case DataType.Money:
+                    _moneyPlayer = dataPlayer.Value;
+                    break;
+
+                case DataType.Health:
+                    _healthPlayer = dataPlayer.Value;
+                    break;
+
+                case DataType.Power:
+                    _powerPlayer = dataPlayer.Value;
+                    break;
+            }
+
+            Debug.Log($"Notified {_name} change to {dataPlayer}");
+        }
+
+        public int CalcPower()
+        {
+            int kHealth = CalcKHealth();
+            float moneyRatio = _moneyPlayer / _kMoney;
+            float powerRatio = _powerPlayer / _kPower;
+
+            return (int)(moneyRatio + kHealth + powerRatio);
+        }
+
+        private int CalcKHealth() =>
+            _healthPlayer > _maxHealthPlayer ? 100 : 5;
+    }
+}
